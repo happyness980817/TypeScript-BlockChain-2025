b381bbd640a57924f039e2c68f48a52a0d00b870
Paul Jang <happyness980817@gmail.com>
2025-06-07 21:48:29 +0900

    blocks

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-import { init, exit } from "./myPackage";
+import crpyto from "crypto";
+
+interface BlockShpae {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShpae {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+  }
+}
